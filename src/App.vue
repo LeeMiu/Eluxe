6720f1f0331f03cd42c3ef37b4b7f7752a222542
biao.li <biao.li@xuncetech.com>
2019-11-15 14:16:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  provide() {
+    return {
+      app: this
+    }
+  },
+}
+</script>
+
+<style lang='less'>
+body {
+  overflow: hidden;
+}
+.common-icon {
+  width: 25px;
+  height: 25px;
+  border: 1px solid #b6bfcd;
+  border-radius: 2px;
+  cursor: pointer;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  background-color: #fff;
+}
+</style>
